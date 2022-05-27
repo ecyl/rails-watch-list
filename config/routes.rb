b95936dfdd3d7259a805abbf9c35f964ba36e532
eunice chin <ecyl2461@gmail.com>
2022-05-27 14:00:55 +0800

    created the unpretty lists feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # see all the lists
+  get '/lists', to: 'lists#index'
+  # show detail of a given list and name
+  get '/lists/new', to: 'lists#new'
+  post '/lists', to: 'lists#create'
+
+  get '/lists/:id', to: 'lists#show', as: :list
+  # new
+  # create
 end
