8ecbed89f8d7c94e3bbdea4c6b2b4195c6964c62
eunice chin <ecyl2461@gmail.com>
2022-05-28 16:20:36 +0800

    added a banner and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # see all the lists
   # get '/lists', to: 'lists#index'
@@ -8,7 +9,6 @@ Rails.application.routes.draw do
   # post '/lists', to: 'lists#create'
   # # show detail of a given list and name
   # get '/lists/:id', to: 'lists#show', as: :list
-
   # add a new bookmark (movie/list) pair
   resources :lists, only: [:index, :new, :create, :show] do
     resources :bookmarks, only: [:new, :create]
