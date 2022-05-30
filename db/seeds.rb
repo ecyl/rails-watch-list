6e2ecd77498667034991bfe6a00f8075e6749099
eunice chin <ecyl2461@gmail.com>
2022-05-30 13:00:46 +0800

    added heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Destroying database...'
+List.destroy_all
+Bookmark.destroy_all
 Movie.destroy_all
 
 puts 'Creating movies...'
